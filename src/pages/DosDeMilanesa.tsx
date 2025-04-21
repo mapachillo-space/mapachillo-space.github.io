7175858e78ea75d6f7bc627ad4066ce310bf1896
supersweet-dev <jim@supersweet.dev>
2025-04-21 08:28:26 -0600

    Translation system set up. Toggle button missing.

--- src/pages/DosDeMilanesa.tsx
+++ src/pages/DosDeMilanesa.tsx
@@ -1,24 +1,24 @@
 import './DosDeMilanesa.css';
 import DDMHeader from '../components/DDM/DDMHeader';
 import InfoCard from '../components/DDM/InfoCard';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
+import pageContent from './PageContent';
+
+i18n.addResourceBundle('es', 'DDMPageContent', pageContent.es);
+i18n.addResourceBundle('en', 'DDMPageContent', pageContent.en);
 
 const DosDeMilanesa = () => {
+  const { t } = useTranslation('DDMPageContent');
   return (
     <div className="dos-de-milanesa">
       <DDMHeader />
       <main className="ddm-content">
-        <InfoCard id="concepto" title="Concepto">
-          El cerdito Miguel tiene un carrito donde vende tortas, pero sus
-          clientes tienen gustos bastante peculiares. Con el paso del tiempo, su
-          carrito se vuelve cada vez más popular: visita nuevas ciudades, llegan
-          más y más clientes, y Miguel empieza a incorporar nuevas tortas e
-          ingredientes. Ahora, Miguel tiene que atenderlos a todos rápidamente y
-          mantenerlos contentos, mientras trata de no desperdiciar comida y
-          maximizar sus ganancias.
+        <InfoCard id="concepto" title={t('concept.title')}>
+          {t('concept.body')}
         </InfoCard>
-
-        <InfoCard id="historia" title="Historia">
-          <p>Aquí puedes añadir la historia del juego...</p>
+        <InfoCard id="controls" title={t('controls.title')}>
+          {t('controls.body')}
         </InfoCard>
       </main>
     </div>
